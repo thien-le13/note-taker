f063435a118b396bd7df7eb6a991c7a0d17c9c6b
Buhdurpadur <thien.le13@yahoo.com>
2023-01-24 22:10:04 -0800

    File created and coded for API routes.

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 // App start and port.
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
 // Route middleware, static, and body parsing.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
