5c9a94e912bc1536bcd2e95a457217f4e90d1beb
Buhdurpadur <thien.le13@yahoo.com>
2023-01-24 18:37:17 -0800

    Added port and middleware.

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,13 @@
 const express = require('express');
 
+// App start and port.
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+// Route middleware, static, and body parsing.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
