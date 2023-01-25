28840e6d00a65ca38c27a7d820d71b4041648a92
Buhdurpadur <thien.le13@yahoo.com>
2023-01-24 22:06:03 -0800

    Coded HTML routes.

--- Develop/server.js
+++ Develop/server.js
@@ -11,3 +11,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
